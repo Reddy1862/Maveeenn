a54914ad0158472f38784636c845229779e2538f
Uma Maheswar Amudam <umamaheswar.amudam@otsi.com>
2019-09-13 12:41:35 +0530

    Maveeennn project commited

---
+++ src/main/java/com/Demo/WebElements.java
@@ -0,0 +1,10 @@
+package com.Demo;
+
+public class WebElements {
+
+	
+	public static String mailId = "//input[@id='identifierId']";
+	public static String mailNext =  "identifierNext";
+	public static String passwords= "//input[@name='password']";
+	public static String passNext = "//div/div[@id='passwordNext']";
+}
